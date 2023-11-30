97d14d0577ddc87fb79bf65fce869063c999f37b
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-30 11:51:34 -0500

    Create helper node

---
+++ HelperNode.c
@@ -0,0 +1,14 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <semaphore.h>
+
+#include <sys/socket.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <arpa/inet.h>
